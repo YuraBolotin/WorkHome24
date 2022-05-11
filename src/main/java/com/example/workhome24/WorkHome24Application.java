35d6481b2b16368e7cfa8293448103e6614d640a
Yura <bolotinya@mail.ru>
2022-05-11 20:14:23 +0300

    Домашнее задание к уроку с исключениями

---
+++ src/main/java/com/example/workhome24/WorkHome24Application.java
@@ -0,0 +1,13 @@
+package com.example.workhome24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHome24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkHome24Application.class, args);
+    }
+
+}
